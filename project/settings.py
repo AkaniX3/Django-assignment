27a6c4c20b3f600951d8b8b5c534066d276e4ada
AkaniX3 <parthrajkondawar@gmail.com>
2025-01-18 23:36:21 +0530

    fixes for render

--- project/settings.py
+++ project/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/project',
         conn_max_age=600
     )
 }
